b5ac692442797d6d1e9d5cc56fed906c3fc5926a
sarthakveeramallu-bit <sarthakveeramallu@gmail.com>
2026-06-27 20:47:56 +0530

    Add basic operations and variable examples in Python

---
+++ snack-shop.py
@@ -0,0 +1,61 @@
+a = 10
+name = "Codingal"
+temp = 11.5
+isstudent = true
+print(type(a))
+print(type(name))
+print(type(temp))
+print(type(isstudent))
+
+
+print(18 + 7)
+print(10 - 5)
+print(10 * 6)
+print(10 / 5)
+print(10 // 2)
+print(100 % 2)
+print(10 ** 2)
+
+
+print(18 = 18)
+print(18 != 7)
+print(18 < 7)
+print(18 > 7)
+print(18 >= 7)
+print(18 <= 7)
+
+
+first = "Codingal"
+last = "website for learing"
+print(first + last)
+
+
+print(hello * 5)
+
+
+word = "programming"
+print(len(word))
+
+
+word = "programming"
+print(word.upper())
+
+
+word = "PROGRAMMING"
+print(word.lower())
+
+
+word = "python"
+print(word(0))
+print(word(2))
+print(word(5))
+
+
+a = 10
+b = 20
+
+temp = a
+a = b
+a =temp
+print(a)
+print(b)
