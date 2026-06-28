a7030a8ba4b17f9ecccc3a242a3bcb58a04566c7
sarthakveeramallu-bit <sarthakveeramallu@gmail.com>
2026-06-28 20:34:30 +0530

    Correct syntax errors and add datetime functionality
    Fix syntax errors and improve date handling in weather-report.py.

---
+++ weather-report.py
@@ -0,0 +1,41 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+print(num1 - num2)
+
+
+age = 15
+
+if = age >=18
+print("You can vote")
+
+else
+print("You are minor")
+
+
+marks = int(input("Enter your marks"))
+
+if marks = 90
+print("Grade A")
+
+elif marks = 75
+print("Grade B")
+
+elif marks = 50
+print("grade C")
+
+elif marks = 35
+print("Pass")
+
+else
+print("Fail")
+
+
+import datetine
+
+today = datetime.datetime.now()
+print(today)
+print(today.time())
+print(today.date())
+print(today.day())
+print(today.month())
+print(today.year())
