2bd45add376e34005e59ca7453b5d5a3d82b4928
sarthakveeramallu-bit <sarthakveeramallu@gmail.com>
2026-07-25 20:48:16 +0530

    Create 6-opertation-list.py

---
+++ 6-opertation-list.py
@@ -0,0 +1,19 @@
+fruits = ["Apple", "Mango", "Grapes", "Banana", "Orange"]
+print("Original List:", fruits)
+
+fruits.append("Pineapple")
+print("After Append:", fruits)
+
+fruits.insert(2 , "Kiwi")
+print("After Insert:", fruits)
+
+fruits.remove("Grapes")
+print("After Remove:", fruits)
+
+fruits.sort()
+print("After Sort:", fruits)
+
+fruits.reverse()
+print("After Revese:", fruits)
+
+print("Total Fruits:", len(fruits))
