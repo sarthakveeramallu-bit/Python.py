f799e0a27ac86898984f1bf6e64308fa58bc4f00
sarthakveeramallu-bit <sarthakveeramallu@gmail.com>
2026-08-01 20:45:13 +0530

    Create 7-Recipe-explorer.py

---
+++ 7-Recipe-explorer.py
@@ -0,0 +1,19 @@
+recpie1 = ("Vegetable Sandwich")
+recpie2 = ("Cheese Snadwich")
+
+ingredients1 = {"Bread", "Butter", "Tomato", "Chesse", "Cucumber"}
+ingredients2 = {"Bread", "Butter", "Chesse", "Onion", "Capsicum"}
+
+print("Recpie 1:", recpie1[0])
+print("Recpie 2:", recpie2[0])
+
+A = {1, 2, 3, 4}
+B = {3, 4, 5, 6}
+
+print(A.union(B))
+
+print(A.intersection(B))
+
+print(A.difference(B))
+
+print(A.symmetric_difference(B))
