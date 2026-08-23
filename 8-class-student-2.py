6ba0f7742ca83b0ddc823c6a6c30b8a6c809be88
sarthakveeramallu-bit <sarthakveeramallu@gmail.com>
2026-08-23 20:43:59 +0530

    Create 8-class-student-2.py

---
+++ 8-class-student-2.py
@@ -0,0 +1,12 @@
+class student:
+    grade = 10
+    name = "Sarthak"
+    def introduce(self):
+        print("Hi I am a student")
+        def details(self):
+            print("My name is", self.name) 
+            print("I am in grade", self.grade)
+
+ob = student()
+ob.introduce()
+ob.details()    
